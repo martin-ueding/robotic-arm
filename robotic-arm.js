f275869be3390b60a2d0febe6e29077720b35e22
Martin Ueding <dev@martin-ueding.de>
2012-12-22 21:32:31 +0100

    Larger, more radii

--- robotic-arm.js
+++ robotic-arm.js
@@ -4,18 +4,20 @@
 /*                                  Options                                  */
 /*****************************************************************************/
 
-var width = 300;
-var height = 200;
-var radius = 20;
-var thickness = 15;
+var width = 600;
+var height = 400;
+var radius = [20, 18, 15];
+var thickness1 = 15;
+var thickness2 = 7;
 
 var circleAttributes = {
-	fill: "r(.5,.0)hsb(0,0,.90)-hsb(0,0,.50)",
+	fill: "white",
+	stroke: "blue",
 };
 
 var armAttributes = {
-	fill: "90-hsb(0,0,.70)-hsb(0,0,.80)",
-	stroke: "black",
+	fill: "white",
+	stroke: "blue",
 };
 
 /*****************************************************************************/
@@ -24,7 +26,7 @@ var armAttributes = {
 
 var first, middle, last, arm1, arm2, text;
 
-var maxrange = width - 2 * radius;
+var maxrange = width - radius[0] - radius[2];
 var range = maxrange / 2;
 
 var armLength = maxrange / 2;
@@ -36,22 +38,22 @@ var angle = function(range) {
 
 var midPos = function(range) {
 	return {
-		x: range / 2 + radius,
-			y: height - Math.sin(angle(range)) * armLength - radius
+		x: range / 2 + radius[0],
+			y: height - Math.sin(angle(range)) * armLength - radius[0]
 	};
 };
 
 var onMove = function(dx, dy, x, y, e) {
-	if (x > width - radius) {
-		x = width - radius;
+	if (x > width - radius[2]) {
+		x = width - radius[2];
 	}
-	else if (x < 3 * radius) {
-		x = 3 * radius;
+	else if (x < 2 * radius[0] + radius[2]) {
+		x = 2 * radius[0] + radius[2];
 	}
 
 	this.attr({cx: x});
 
-	range = x - radius;
+	range = x - radius[0];
 
 	p = midPos(range);
 
@@ -77,12 +79,12 @@ var setTransform = function(range) {
 
 	arm1.transform("t-"+armLength/2+",0r-"+a+"t"+armLength/2+",0");
 	mid = midPos(range);
-	arm2.attr({x: mid.x, y: mid.y - thickness/2});
+	arm2.attr({x: mid.x, y: mid.y - thickness2/2});
 	arm2.transform("t-"+armLength/2+",0r"+a+"t"+armLength/2+",0");
 };
 
 var realRangePart = function(range) {
-	return (range-2*radius)/(maxrange-2 *radius);
+	return (range-radius[0]-radius[2])/(maxrange-radius[0]-radius[2]);
 };
 
 var updateText = function(range) {
@@ -94,19 +96,19 @@ var main = function() {
 	
 	var initMidPos = midPos(range);
 
-	var x1 = radius;
-	var y1 = height-radius;
+	var x1 = radius[0];
+	var y1 = height-radius[0];
 	var x2 = initMidPos.x;
 	var y2 = initMidPos.y;
-	var x3 = range + radius;
-	var y3 = height-radius;
+	var x3 = range + radius[0];
+	var y3 = height-radius[0];
 
-	arm1 = paper.rect(x1, y1-thickness/2, maxrange/2, thickness).attr(armAttributes);
-	arm2 = paper.rect(x2, y2-thickness/2, maxrange/2, thickness).attr(armAttributes);
+	arm1 = paper.rect(x1, y1-thickness1/2, maxrange/2, thickness1).attr(armAttributes);
+	arm2 = paper.rect(x2, y2-thickness2/2, maxrange/2, thickness2).attr(armAttributes);
 
-	first = paper.circle(x1, y1, radius).attr(circleAttributes);
-	middle = paper.circle(x2, y2, radius).attr(circleAttributes);
-	last = paper.circle(x3, y3, radius).attr(circleAttributes);
+	first = paper.circle(x1, y1, radius[0]).attr(circleAttributes);
+	middle = paper.circle(x2, y2, radius[1]).attr(circleAttributes);
+	last = paper.circle(x3, y3, radius[2]).attr(circleAttributes);
 
 	last.attr({cursor: "move"});
 
